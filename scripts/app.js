75b650e7fedb8f9ca4a2b6d0334dd0d5b5f071dc
NikaL25 <77246013+NikaL25@users.noreply.github.com>
2022-09-29 03:52:46 +0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,66 @@
-(function() {
+(function () {
 'use strict';
 
-angular.module('appModule1', [])
+angular.module('LunchCheck', [])
 
-    .controller('appControllerModule1', function($scope) {
-        
-        $scope.name = [];
-        $scope.message = '';
-        $scope.classChecker = null;
-        $scope.whiteSpaceChecker = false;
+.controller('LunchCheckerController', LunchCheckerController)
 
-        $scope.getItems = function() {
-            if (!$scope.name || !$scope.name.length) {
-                $scope.message = 'Please enter data first.';
-                $scope.classChecker = false;
-            } else {
-                var tempTotal = $scope.name.split(',');
-                $scope.classChecker = true;
-                    if (tempTotal.length > 3) {
-                        $scope.message = 'Too much!';
-                    } else {
-                        $scope.message = 'Enjoy';
-                    }
-            }
+LunchCheckerController.$inject = ['$scope']
+
+function LunchCheckerController($scope) {
+  var maxCountOfDishes = 3;
+  $scope.userMessage = "";
+  $scope.listOfLunchItems = "";
+  $scope.errorMessage = "";
+
+  $scope.checkIfTooMuch = function () {
+    var resultValidation =  validateListOfItems($scope.listOfLunchItems)
+
+    if (resultValidation.error)
+    {
+      $scope.userMessage = "";
+      $scope.errorMessage = resultValidation.message;
+      return;
+    }
+
+    var message = getLunchValueVerdict($scope.listOfLunchItems);
+    $scope.userMessage = message;
+    $scope.errorMessage = "";
+  }
+
+ function  validateListOfItems(listOfitems) {
+    var result = { error: false, message: "" };
+
+    if (listOfitems.length == 0)
+    {
+      result.error = true;
+      result.message = "Please enter data first";
+    }
+    else {
+      var arrayOfDishes = listOfitems.split(',');
+      for (var i = 0; i < arrayOfDishes.length; i++) {
+        if (arrayOfDishes[i].length == 0)
+        {
+          result.error = true;
+          result.message = "Please, input correct list of dishes. No empty, use comma separeter.";
         }
-    });
+      }
+    }
+
+    return result;
+ }
+
+  function getLunchValueVerdict(listOfitems) {
+
+      var countOfDishes = listOfitems.split(',').length;
+
+      if (countOfDishes > maxCountOfDishes) {
+        return "Too much!";
+      }
+      else {
+       return "Enjoy!";
+      }
+  }
+};
 
 })();
